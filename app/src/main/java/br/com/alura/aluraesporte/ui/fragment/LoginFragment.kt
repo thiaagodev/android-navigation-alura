332a5ebbc4dc439f1d9d39843df44ccaf671f8a0
Thiago Ferreira <thiaago.dev@gmail.com>
2023-01-11 20:35:58 -0300

    adicionando a tela de login

---
+++ app/src/main/java/br/com/alura/aluraesporte/ui/fragment/LoginFragment.kt
@@ -0,0 +1,35 @@
+package br.com.alura.aluraesporte.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import br.com.alura.aluraesporte.R
+import kotlinx.android.synthetic.main.login.login_botao_logar
+
+class LoginFragment() : Fragment() {
+
+    private val controlador by lazy {
+        findNavController()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        login_botao_logar.setOnClickListener {
+            val direcao = LoginFragmentDirections.actionLoginToListaProdutosFragment()
+            controlador.navigate(direcao)
+        }
+    }
+
+}
