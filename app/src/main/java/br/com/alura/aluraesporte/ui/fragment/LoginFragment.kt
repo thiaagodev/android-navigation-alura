85671a4fd324d6bcae765722b49bcd26a8f50b8a
Thiago Ferreira <thiaago.dev@gmail.com>
2023-01-12 20:41:25 -0300

    configurando a primeira com condição

--- app/src/main/java/br/com/alura/aluraesporte/ui/fragment/LoginFragment.kt
+++ app/src/main/java/br/com/alura/aluraesporte/ui/fragment/LoginFragment.kt
@@ -7,13 +7,17 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import br.com.alura.aluraesporte.R
+import br.com.alura.aluraesporte.ui.viewmodel.LoginViewModel
 import kotlinx.android.synthetic.main.login.login_botao_logar
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class LoginFragment() : Fragment() {
 
     private val controlador by lazy {
         findNavController()
     }
+
+    private val viewModel: LoginViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,12 +28,22 @@ class LoginFragment() : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        if (viewModel.estaLogado()) {
+            vaiParaListaProdutos()
+        }
+
         super.onViewCreated(view, savedInstanceState)
 
         login_botao_logar.setOnClickListener {
-            val direcao = LoginFragmentDirections.actionLoginToListaProdutosFragment()
-            controlador.navigate(direcao)
+            viewModel.loga()
+            vaiParaListaProdutos()
         }
+    }
+
+    private fun vaiParaListaProdutos() {
+        val direcao = LoginFragmentDirections.actionLoginToListaProdutosFragment()
+        controlador.navigate(direcao)
     }
 
 }
