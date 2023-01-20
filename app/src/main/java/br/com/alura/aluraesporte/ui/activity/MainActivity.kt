b74a63292d2332538cc30d5a2c41e0f1fc072e8c
Thiago Ferreira <thiaago.dev@gmail.com>
2023-01-20 20:45:25 -0300

    implementando o Bottom Navigation

--- app/src/main/java/br/com/alura/aluraesporte/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/alura/aluraesporte/ui/activity/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import br.com.alura.aluraesporte.R
 import br.com.alura.aluraesporte.ui.viewmodel.EstadoAppViewModel
+import kotlinx.android.synthetic.main.main_activity.main_activity_bottom_navigation
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
@@ -32,6 +34,8 @@ class MainActivity : AppCompatActivity() {
                 }
             })
         }
+
+        main_activity_bottom_navigation.setupWithNavController(controlador)
     }
 
 }
